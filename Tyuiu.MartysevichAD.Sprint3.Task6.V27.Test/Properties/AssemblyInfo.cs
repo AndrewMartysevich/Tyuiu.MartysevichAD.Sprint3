0e51c0caa427ecd0b35b7b316af53f25adabde01
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-10-29 23:36:40 +0500

    Task6

---
+++ Tyuiu.MartysevichAD.Sprint3.Task6.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MartysevichAD.Sprint3.Task6.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MartysevichAD.Sprint3.Task6.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcff8f2f-d50b-41a3-9d95-7069c8e77a44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MartysevichAD.Sprint3.Task6.V27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MartysevichAD.Sprint3.Task6.V27.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint3.Task6.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Мартысевич А. Д. | ИИПб-23-3";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах               *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #27                                                            *");
+            Console.WriteLine("* Выполнил: Мартысевич Андрей Денисович | ИИПб-23-3                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих     * ");
+            Console.WriteLine("* числовому отрезку [14, 20] сумму всех делителей больше 9.              *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************"); ;
+            int startValue = 14;
+            int stopValue = 20;
+            Console.WriteLine("Начало отрезка " + startValue);
+            Console.WriteLine("Конец отрезка " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Сумма делителей " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
